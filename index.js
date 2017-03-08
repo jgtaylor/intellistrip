79832385516202e9b36494534e1d8e47d09f929a
jgtaylor <sonicbphuct@gmail.com>
2017-03-08 20:26:58 +0100

    maybe it works?

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 //var db = require("./bin/lib/influxdbHandler");
+const later = require( 'later' ),
+	moment = require( 'moment' ),
+	websockets = require( 'websockets' ),
+	bone = require( 'bonescript' ),
+	Devices = require( './bin/lib/Devices' ),
+	Event = require( 'events' );
+
+// get the devices available
+var devices = Devices();
+
+// get the logical layout
+var zones = Zones(); // NOT WHAT I WANT, but i don't know what i want :-(
+//zones[0].lights.on.schedule()
+//zones[0].lights.off.schedule()
+//zones[0].pumpX.on.schedule()
+//zones[0].monitor("temp")
+//zones[0].monitor("humidity")
+//zones[0].monitor("lights")
+//zones[0].monitor("water")
