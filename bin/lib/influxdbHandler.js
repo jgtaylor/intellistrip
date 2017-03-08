8e3b982f042ef6c2dc7ef3ec7b718c65e47ce2f2
jgtaylor <sonicbphuct@gmail.com>
2017-03-08 19:07:44 +0100

    first draft, not useable

--- bin/lib/influxdbHandler.js
+++ bin/lib/influxdbHandler.js
@@ -1,28 +1,29 @@
-var http = require( "http" ),
-	options = {
-		hostname: 'automation.local',
-		port: 8086,
-		path: '/write?db=environ',
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json'
-		}
-	},
-	req = function () {
-		return http.request( options, function ( res ) {
-			//            console.log('Status: ' + res.statusCode);
-			//            console.log('Headers: ' + JSON.stringify(res.headers));
-			res.setEncoding( 'utf8' );
-			res.on( 'data', function ( body ) {
-				console.log( 'Body: ' + body );
+module.exports = (msg, opt) => {
+	let http = require( "http" );
+	if (!opt) {
+		options = {
+			hostname: 'automation.local',
+			port: 8086,
+			path: '/write?db=environ',
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		};
+	}
+	let req = function () {
+			return http.request( options, function ( res ) {
+				res.setEncoding( 'utf8' );
+				res.on( 'data', function ( body ) {
+					console.log( 'Body: ' + body );
+				} );
 			} );
-		} );
-	};
+		};
 
-var msg = 'fan,uuid=nice_fan,zone=bloom_tent value=' + fspd;
-var hReq = req();
-hReq.on( 'error', function ( e ) {
-	console.log( e )
-} );
-hReq.write( msg );
-hReq.end();
+	var hReq = req();
+	hReq.on( 'error', function ( e ) {
+		console.log( e )
+	} );
+	hReq.write( msg );
+	hReq.end();
+}
