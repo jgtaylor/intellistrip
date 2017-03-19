6ca5a8d0c35cca4776f03de5281d25c6ca1e84d6
jgtaylor <sonicbphuct@gmail.com>
2017-03-19 12:54:01 +0100

    middle of scheduling - totally will not function as intended.

--- bin/lib/Zone.js
+++ bin/lib/Zone.js
@@ -1,7 +1,8 @@
 "use strict";
 /* eslint no-console: "off", no-unused-vars: "off" */
 /* this is a module, it should return an array of devices */
-const EventEmitter = require( "events" );
+const EventEmitter = require( "events" ),
+	later = require( "later" );
 
 function guid() {
 	return s4() + s4() + "-" + s4() + "-" + s4() + "-" +
@@ -14,20 +15,72 @@ function s4() {
 		.substring( 1 );
 }
 
+function camelCase( str ) {
+	return str.charAt( 0 )
+		.toUpperCase() + str.slice( 1 );
+}
+
+function isValidMethod( fn ) {
+	return typeof fn === "function";
+}
+
 module.exports = ( devices, zoneConfig ) => {
 	let zone = new EventEmitter();
 	if ( !zoneConfig.zoneID ) {
 		zoneConfig.zoneID = guid();
 	}
 	Object.keys( zoneConfig )
-		.forEach( ( key ) => {
+		.forEach( ( a ) => {
 			// use devices.query(zoneConfig[key]) to find it and load it.
 			// once devices.query() works.
-			if ( devices.list().includes(zoneConfig[key])  ) {
-				zone[key] = devices.dev[zoneConfig[key]];
-			} else {
-				zone[key] = zoneConfig[key];
+			if ( devices.list()
+				.includes( zoneConfig[ a ] ) ) {
+				zone[ a ] = devices.dev[ zoneConfig[ a ] ];
 			}
-		});
+			switch ( a ) {
+			case "schedules":
+				{
+					if ( !zone.schedules ) {
+						zone.schedules = {
+							timers: []
+						};
+					}
+						// times is an object of schedules.
+					Object.keys( zoneConfig[ a ].schedules )
+						.forEach( ( b ) => {
+							// zones.zone['uuid'].schedules.light || ...
+							//zone.schedules[el].
+							// build a schedule from the key.key ...
+							// el = light/fogger/pump, etc.
+							let me = zoneConfig[ a ].schedules[ b ];
+							zone.schedules[ b ] = {};
+							Object.keys( me )
+								.forEach( ( c ) => {
+									// c = "on", "off"
+									if ( c !== "referingTo" ) {
+										// TODO: check that the keys are names for the device methods. - another day.
+										// can only work on buttons at this point :-(
+										me.referingTo.forEach( ( obj ) => {
+											let dev = devices.dev[ zoneConfig[ a ][ obj ] ];
+											if ( isValidMethod( dev.button[ c ] ) || isValidMethod(
+													dev.dimmer[ c ] || isValidMethod( dev.virtual[ c ] ) ) ) {
+												zone.schedules[ b ][ c ] = later.schedule( me[ c ] );
+											}
+										} );
+
+									}
+								} );
+							// now we create methods for dealing with the schedules.
+							zone.schedules.start = ( sched, refMethod ) => {
+								if ( zone.schedules.timers instanceof Array ) {
+									zone.schedules.timers.push(  later.setInterval( refMethod, sched ) );
+								}
+							};
+						} ); // end forEach(b)
+					break;
+				}
+			}
+			zone[ a ] = zoneConfig[ a ];
+		} );
 	return zone;
 };
