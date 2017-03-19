feb1cffd8a82b4b9faad68e0d3578f39a6659c08
jgtaylor <sonicbphuct@gmail.com>
2017-03-19 12:54:51 +0100

    format and style conform

--- bin/lib/influxdbHandler.js
+++ bin/lib/influxdbHandler.js
@@ -1,29 +1,29 @@
 module.exports = (msg, opt) => {
 	let http = require( "http" );
 	if (!opt) {
-		options = {
-			hostname: 'automation.local',
+		var options = {
+			hostname: "automation.local",
 			port: 8086,
-			path: '/write?db=environ',
-			method: 'POST',
+			path: "/write?db=environ",
+			method: "POST",
 			headers: {
-				'Content-Type': 'application/json'
+				"Content-Type": "application/json"
 			}
 		};
 	}
 	let req = function () {
-			return http.request( options, function ( res ) {
-				res.setEncoding( 'utf8' );
-				res.on( 'data', function ( body ) {
-					console.log( 'Body: ' + body );
-				} );
+		return http.request( options, function ( res ) {
+			res.setEncoding( "utf8" );
+			res.on( "data", function ( body ) {
+				console.log( "Body: " + body );
 			} );
-		};
+		} );
+	};
 
 	var hReq = req();
-	hReq.on( 'error', function ( e ) {
-		console.log( e )
+	hReq.on( "error", function ( e ) {
+		console.log( e );
 	} );
 	hReq.write( msg );
 	hReq.end();
-}
+};
