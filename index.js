a06f7c5a34f469c5b73be7176e0c7efcd03b13d7
jgtaylor <sonicbphuct@gmail.com>
2017-03-20 12:44:10 +0100

    register listeners, initialize outputs

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 "use strict";
+/* eslint no-unused-vars: "off", no-console: "off" */
 //var db = require("./bin/lib/influxdbHandler");
 var later = require( "later" ),
 	moment = require( "moment" ),
@@ -8,6 +9,35 @@ var later = require( "later" ),
 	devices = Devices(),
 	zones = Zones( devices );
 
+// we need to register listeners here...
+zones.list()
+	.forEach( ( z ) => {
+		Object.keys( zones.zone[ z ].things )
+			.forEach( ( t ) => {
+				if ( zones.zone[ z ].things[ t ].button ) {
+					zones.zone[ z ].things[ t ].on( "init", ( msg ) => {
+						console.log( `${t} init: ${msg}` );
+					} );
+					zones.zone[ z ].things[ t ].on( "state", ( msg ) => {
+						console.log( `${t} state: ${msg}` );
+					} );
+					zones.zone[ z ].things[ t ].on( "status", ( msg ) => {
+						console.log( `${t} status: ${msg}` );
+					} );
+				}
+			} );
+	} );
+// we should initialize zone devices with an init() method.
+zones.list()
+	.forEach( ( z ) => {
+		Object.keys( zones.zone[ z ].things )
+			.forEach( ( t ) => {
+				if ( Object.keys( zones.zone[ z ].things[ t ].button )
+					.includes( "init" ) ) {
+					zones.zone[ z ].things[ t ].button.init();
+				}
+			} );
+	} );
 // start schedules
 zones.list()
 	.forEach( ( z ) => {
@@ -15,10 +45,11 @@ zones.list()
 			.forEach( ( sched ) => {
 				zones.zone[ z ].schedules.run( sched );
 			} );
-		console.log( zones.zone[ z ].schedules.listRunning() );
+		// we will start our monitors here, when monitors are here :-)
+		console.log( `Completed startup for zone: ${zones.zone[z].zoneName}.\nSchedules, ${zones.zone[ z ].schedules.listRunning().join(", ")} have been started.` );
 	} );
 
-console.log( zones );
+
 
 // get the logical layout
 //var zones = Zones(); // NOT WHAT I WANT, but i don't know what i want :-(
