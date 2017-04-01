1d53a26bb084e01225f9fae900a5e42e3e5566bf
root <root@arm.localdomain>
2017-04-01 23:35:21 +0000

    check for msg - fails

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ zones.list()
 					zones.zone[ z ].things[ t ].on( "read", ( msg ) => {
 						// {"celsius":"22.7","fahrenheit":"72.9","humidity":"38.5"}
 						// "fan,uuid=nice_fan,zone=bloom_tent value=" + fspd;
-						if ( msg.celsius ) {
+						if ( msg ) {
 							let data = influx(
 								`${zones.zone[z].things[t].meta.outputs[0].metric},uuid=${zones.zone[z].things[t].deviceID},zone=${zones.zone[z].zoneName},zoneID=${z},unit="${zones.zone[z].things[t].meta.outputs[0].unit}" val=${msg.celsius}\n` +
 								`${zones.zone[z].things[t].meta.outputs[1].metric},uuid=${zones.zone[z].things[t].deviceID},zone=${zones.zone[z].zoneName},zoneID=${z},unit="${zones.zone[z].things[t].meta.outputs[1].metric}" val=${msg.humidity}`
